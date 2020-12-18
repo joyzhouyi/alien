6ed9859318ecf3c3b66703fe92d1da3b7ef1632f
joyzhouyi <2643983350@qq.com>
2020-12-18 15:57:08 +0800

    Default Changelist

--- settings.py
+++ settings.py
@@ -7,14 +7,14 @@ class Settings():
         self.screen_height=600
         self.bg_color=(60,63,65)
         #飞船的设置
-        self.ship_speed_factor=3
+        self.ship_speed_factor=1
         self.ship_limit=3
         #子弹的设置
-        self.bullet_speed_factor=3
+        self.bullet_speed_factor=10
         self.bullet_width=3
         self.bullet_height=12
-        self.bullet_color=100,216,238
-        self.bullets_allowed=6
+        self.bullet_color=100,111,238
+        self.bullets_allowed=16
         #外星人设置
         self.alien_speed_factor=1
         self.fleet_drop_speed=5
@@ -39,13 +39,13 @@ class Settings():
         self.ship_speed_factor=1.5
         self.bullet_speed_factor=3
         self.alien_speed_factor=1
-
+        self.ship_speed_factor = 4
+        self.bullet_speed_factor = 5
         #fleet_direction为1表示向右；为-1表示向左
         self.fleet_direction=1
         #计分
         self.alien_points=50
     def increase_speed(self):
-
         """提供速度设置和外星人点数"""
         self.ship_speed_factor*=self.speedup_scale
         self.bullet_speed_factor*=self.speedup_scale
